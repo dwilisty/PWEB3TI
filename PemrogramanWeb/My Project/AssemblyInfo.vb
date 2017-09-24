e799a73394c71a51b89f6c2b455b82431bf43156
dwilisty <dwilisty@pcr.ac.id>
2017-09-24 16:26:34 +0700

    Add project files.

---
+++ PemrogramanWeb/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("PemrogramanWeb")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("PemrogramanWeb")>
+<Assembly: AssemblyCopyright("Copyright ©  2017")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f0df4f86-a759-4209-a8ee-dd1166a1f04d")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
---
+++ PemrogramanWeb/Pertemuan1/WebPertama.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="vb" AutoEventWireup="false" CodeBehind="WebPertama.aspx.vb" Inherits="PemrogramanWeb.WebPertama" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Size="XX-Large" Text="PEMROGRAMAN WEB 2 - 3 TI"></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
